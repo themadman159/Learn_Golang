18472e3285720813b7be47a3d4e470ab5934d3f1
themadman159 <chaibig55@gmail.com>
2024-04-07 14:52:57 +0700

    connecting to server

--- Databases/migration/migration.go
+++ Databases/migration/migration.go
@@ -1,1 +1,16 @@
-package migration
+package main
+
+import (
+	"fmt"
+
+	"github.com/themadman159/go-tutor/config"
+	databases "github.com/themadman159/go-tutor/databases"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDB(conf.Database)
+
+	fmt.Println(db.ConnectionGetting())
+
+}
--- Databases/postgresDB.go
+++ Databases/postgresDB.go
@@ -1,1 +1,48 @@
 package databases
+
+import (
+	"fmt"
+	"sync"
+	"log"
+
+	"github.com/themadman159/go-tutor/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type PostgresDB struct {
+	*gorm.DB
+}
+
+var (
+	postgresDBInstance *PostgresDB
+	once               sync.Once
+)
+
+func NewPostgresDB(conf *config.Database) Database {
+
+	once.Do(func() {
+		dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s search_path=%s",
+			conf.Host,
+			conf.Port,
+			conf.User,
+			conf.Password,
+			conf.DBName,
+			conf.SSLMode,
+			conf.Schema,
+		)
+
+		conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("connect to database %s", conf.DBName)
+		postgresDBInstance = &PostgresDB{conn}
+	})
+	return postgresDBInstance
+}
+
+func (db *PostgresDB) ConnectionGetting() *gorm.DB {
+	return db.DB
+}
