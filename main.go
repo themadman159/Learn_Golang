0a41c57e71b6ae232a3f50f40ca25c3a9bc3c3a7
themadman159 <chaibig55@gmail.com>
2024-04-07 15:18:16 +0700

    serve http with echo

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/themadman159/go-tutor/config"
+	databases "github.com/themadman159/go-tutor/databases"
+	"github.com/themadman159/go-tutor/server"
+)
+
 func main() {
-	
+
+	conf := config.ConfigGetting()
+
+	db := databases.NewPostgresDB(conf.Database)
+
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,62 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"github.com/themadman159/go-tutor/config"
+	"gorm.io/gorm"
+)
+
+type echoServer struct {
+	app  *echo.Echo
+	db   *gorm.DB
+	conf *config.Config
+}
+
+var (
+	once   sync.Once
+	server *echoServer
+)
+
+func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
+	echoApp := echo.New()
+	echoApp.Logger.SetLevel(log.DEBUG)
+
+	once.Do(func() {
+		server = &echoServer{
+			app:  echoApp,
+			db:   db,
+			conf: conf,
+		}
+	})
+
+	return server
+}
+
+func (s *echoServer) Start() {
+
+	s.app.GET("/v1/health", s.healthCheck)
+
+	s.httpListening()
+
+}
+
+func (s *echoServer) httpListening() {
+
+	serverURL := fmt.Sprintf(":%d", s.conf.Server.Port)
+
+	if err := s.app.Start(serverURL); err != nil && err != http.ErrServerClosed {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
+
+}
+
+func (s *echoServer) healthCheck(c echo.Context) error {
+
+	return c.String(http.StatusOK, "OK")
+
+}
